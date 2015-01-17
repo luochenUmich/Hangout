4de87faca0ec487972da69269ae93853306cb3df
Chen Luo <luochen@umich.edu>
2015-01-17 10:40:32 -0500

    change construct

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'askldlk2913aius0a0zA09adH29110F';
 
 /*
 |--------------------------------------------------------------------------
